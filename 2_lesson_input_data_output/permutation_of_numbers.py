fe2483c36325f1f347a31ffeb357df1a7b824118
Delakroa <katon1986@gmail.com>
2022-11-08 10:33:45 +0300

    Задача решено

--- 2_lesson_input_data_output/permutation_of_numbers.py
+++ 2_lesson_input_data_output/permutation_of_numbers.py
@@ -10,3 +10,14 @@
 abc,acb,bac,bca,cab,cba."""
 
 numbers = int(input())
+data = numbers // 10
+a = data // 10
+b = data % 10
+c = numbers % 10
+
+print(f'{a}{b}{c}')
+print(f'{a}{c}{b}')
+print(f'{b}{a}{c}')
+print(f'{b}{c}{a}')
+print(f'{c}{a}{b}')
+print(f'{c}{b}{a}')
